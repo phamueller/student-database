1183d79408cc7aa5986d2d5c73ea8697bdee8bbd
phamueller <phamueller@outlook.com>
2023-10-20 20:26:42 +0200

    update

--- student_db/student_db.sql
+++ student_db/student_db.sql
@@ -28,6 +28,7 @@ INSERT INTO student (student_name, student_number, student_class, student_major)
 ('John Rob', 55, 'Data modelling and database programming', 'Informatics'),
 ('Krish Star', 60, 'Data modelling and database programming', 'Informatics');
 
+SELECT * FROM student;
 
 DROP TABLE IF EXISTS course;
 
@@ -38,8 +39,8 @@ CREATE TABLE course (
     course_name varchar(45),
     -- course_department int,
     course_department varchar(45),
-    PRIMARY KEY (course_id), 
-    FOREIGN KEY (course_id) REFERENCES student(student_id)
+    PRIMARY KEY (course_id) 
+    -- FOREIGN KEY (course_id) REFERENCES student(student_id)
 );
 
 INSERT INTO course (course_number, course_hours, course_department) VALUES
@@ -47,3 +48,36 @@ INSERT INTO course (course_number, course_hours, course_department) VALUES
 ('DSPRAXP300', 150, 'Practical project III', 'Informatics'),
 
 SELECT * FROM course;
+
+DROP TABLE IF EXISTS student_course;
+
+CREATE TABLE student_course (
+	student_course_id int NOT NULL AUTO_INCREMENT,
+	course_id int,
+	student_id int,
+	-- date,
+	PRIMARY KEY (student_course_id),
+	FOREIGN KEY (course_id) REFERENCES course(course_id),
+	FOREIGN KEY (student_id) REFERENCES student(student_id)
+);
+
+INSERT INTO student_course (student_id, course_id) VALUES
+(1,1),
+(1,2),
+(2,1),
+(2,2),
+(3,1),
+(3,2),
+(4,1),
+(4,2);
+
+SELECT * FROM student_course;
+
+SELECT 
+	s.*, 
+	c.* 
+FROM student s 
+JOIN student_course sc ON s.student_id = sc.student_id -- INNER JOIN, OUTER JOIN, LEFT JOIN, RIGHT JOIN 
+JOIN course c ON c.course_id = sc.course_id
+WHERE c.course_id = 1
+ORDER BY s.student_number ASC; -- DESC
