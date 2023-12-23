3f1d64c5bf32aeca5a80f14c0cec8b3441f0092d
phamueller <phamueller@outlook.com>
2023-12-23 12:09:03 +0100

    update

---
+++ student_db/student_db-02-verbundmengen.sql
@@ -0,0 +1,113 @@
+USE student_db;
+
+DROP TABLE IF EXISTS artikel;
+
+CREATE TABLE artikel (
+  artikel_id int NOT NULL AUTO_INCREMENT,
+  artikel_nr varchar(45),
+  preis double,
+  name varchar(45),
+  verfuegbarkeit varchar(45),
+  PRIMARY KEY (artikel_id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- SHOW VARIABLES LIKE 'auto_inc%';
+-- ALTER TABLE artikel AUTO_INCREMENT = 1;
+
+INSERT INTO artikel (artikel_nr, preis, name, verfuegbarkeit) VALUES
+('UZEHN78126',10.99,'Auf dem Weg ins Nichts','sofort'),
+('ZHBH789123',13.99,'Hinter uns','sofort'),
+('KJNSM7873',9.99,'Help you','in 3 Tagen'),
+('UIHJKHSD7',8.99,'Around the Planet','in 2 Tagen');
+
+SELECT * FROM artikel;
+
+DROP TABLE IF EXISTS film;
+
+CREATE TABLE film (
+  film_id int NOT NULL AUTO_INCREMENT,
+  regisseur	varchar(45),
+  sprachen varchar(45),
+  untertitel varchar(45),
+  anzahl_discs int,
+  studio varchar(45),
+  medium varchar(45),
+  artikel_id int,
+  PRIMARY KEY (film_id),
+  FOREIGN KEY (artikel_id) REFERENCES artikel(artikel_id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8; 
+
+INSERT INTO film (regisseur, sprachen, untertitel, anzahl_discs, studio, medium, artikel_id) VALUES
+('Sophie Little','DE, EN, FR','DE, EN', 1, 'DreamPictures','DVD',1),
+('Fred van Heldt','EN, FR','EN, FR, DE', 2, 'MovieFactory','DVD',2);
+
+SELECT * FROM film;
+
+
+/** JOINs **/
+SELECT 
+	a.name, 
+	f.regisseur,
+	a.preis,
+	a.verfuegbarkeit	
+FROM artikel a 
+JOIN film f ON a.artikel_id = f.artikel_id
+ORDER by a.preis;
+
+SELECT 
+	a.*, f.*
+FROM artikel a 
+JOIN film f ON a.artikel_id = f.artikel_id;
+
+SELECT 
+	anzahl_Discs,
+	-- verfuegbarkeit 
+	COUNT(*) AS Anzahl 
+FROM artikel a 
+JOIN film f ON a.artikel_id = f.artikel_id;
+
+SELECT 
+	anzahl_Discs,
+	COUNT(*) AS Anzahl 
+FROM artikel NATURAL JOIN film
+WHERE verfuegbarkeit = 'sofort' 
+AND anzahl_discs > 1 
+GROUP BY anzahl_discs ;
+
+-- Inner JOIN mit ON
+SELECT *
+FROM artikel 
+INNER JOIN film
+ON artikel.artikel_id=film.artikel_id;
+
+
+-- Natural JOIN
+SELECT * 
+FROM artikel
+NATURAL JOIN film;
+
+
+-- Inner JOIN mit USING
+SELECT * 
+FROM artikel 
+INNER JOIN film USING(artikel_id);
+
+
+/** VIEWs **/
+DROP VIEW IF EXISTS v_sofort_verfuegbare_filme;
+
+CREATE VIEW v_sofort_verfuegbare_filme AS 
+	SELECT * 
+	FROM artikel a INNER JOIN film f USING(artikel_id)
+	WHERE a.verfuegbarkeit = 'sofort' 
+	ORDER BY a.name;
+
+SELECT * 
+FROM v_sofort_verfuegbare_filme
+WHERE sprachen LIKE '%DE%' OR sprachen LIKE 'DE%';
+
+ALTER VIEW v_sofort_verfuegbare_filme AS 
+	SELECT * 
+	FROM artikel a INNER JOIN film f USING(artikel_id)
+	WHERE a.verfuegbarkeit = 'in 3 Tagen' 
+	ORDER BY a.name;	
---
+++ student_db/student_db-03-rekursion.sql
@@ -0,0 +1,141 @@
+USE student_db;
+
+-- Allgemeines CTE-Schema
+WITH [RECURSIVE] with_query [, ...]
+SELECT ...
+
+
+DROP TABLE IF EXISTS personal;
+
+CREATE TABLE personal (
+	personal_id INTEGER AUTO_INCREMENT,
+	level int,
+	name VARCHAR (45) NOT NULL,
+	manager_id int,	
+	PRIMARY KEY (personal_id) 	
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO personal (level, name, manager_id) VALUES 
+(0, 'Veronika', NULL),
+(1, 'Hans', 1),
+(2, 'Bernd', 2),
+(2, 'Nina', 2);
+
+
+SELECT * FROM personal;
+
+DROP VIEW IF EXISTS v_personal_topmanager;
+CREATE VIEW v_personal_topmanager AS (
+	SELECT 
+		personal_id, 
+		name, 
+		manager_id 
+	FROM personal WHERE manager_id IS NULL	
+);
+SELECT * FROM v_personal_topmanager;
+
+
+-- Beispiel einer Hierarchieabbildung
+SELECT 
+	level, LPAD (' ', 2 * (level- 1)) || name AS 'employee', 
+	personal_id, 
+	manager_id AS 'manager'
+FROM personal 
+START WITH manager_id IS NULL
+CONNECT BY PRIOR personal_id = manager_id;
+-- Connect By wird von MariaDB nicht unterstützt
+
+
+-- Eine rekursive Abfrage zur Abbildung der Hierarchieebenen
+WITH RECURSIVE HierarchicalCTE (level, name, personal_id, manager_id) AS (
+  SELECT
+    p1.level,
+	CONCAT(LPAD(' ', 2 + p1.level), p1.name),
+    -- LPAD(p1.name, 2, '.'),
+	-- p1.name,
+    p1.personal_id,
+    p1.manager_id
+  FROM
+    personal p1
+  WHERE
+    p1.manager_id IS NULL
+  UNION ALL
+  SELECT
+    p2.level,
+	CONCAT(LPAD(' ', 2 + p2.level), p2.name),
+    -- p2.name,
+    p2.personal_id,
+    p2.manager_id
+  FROM
+    personal p2
+  INNER JOIN
+    HierarchicalCTE cte ON p2.manager_id = cte.personal_id
+)
+SELECT 
+	level, 
+	name, 
+	personal_id, 
+	manager_id
+FROM
+	HierarchicalCTE
+ORDER BY
+	level, personal_id;
+
+
+DROP VIEW IF EXISTS v_personal_hierarchie;
+CREATE VIEW v_personal_hierarchie AS (
+	SELECT
+	  e1.personal_id,
+	  e1.level,
+	  e1.name,
+	  e1.manager_id
+	FROM
+	  personal e1
+	-- ein JOIN pro Hierarchieebenen
+	LEFT JOIN
+	  personal e2 ON e1.manager_id = e2.personal_id
+	LEFT JOIN
+	  personal e3 ON e2.manager_id = e3.personal_id
+	LEFT JOIN
+	  personal e4 ON e3.manager_id = e4.personal_id	
+	ORDER BY
+	  e1.level, e1.personal_id
+);
+
+
+DROP VIEW IF EXISTS v_personal_hierarchie_rec;
+CREATE VIEW v_personal_hierarchie_rec AS 
+	WITH RECURSIVE PersonalCTE AS (
+	  SELECT
+	    personal_id,
+	    level,
+	    name,
+	    manager_id
+	  FROM
+	    personal
+	  WHERE
+	    manager_id IS NULL -- Für die oberste Hierarchieebene
+	
+	  UNION ALL
+	
+	  SELECT
+	    p.personal_id,
+	    p.level,
+	    p.name,
+	    p.manager_id
+	  FROM
+	    personal p
+	  INNER JOIN
+	    PersonalCTE cte ON p.manager_id = cte.personal_id
+	)
+	SELECT
+	  personal_id,
+	  level,
+	  name,
+	  manager_id
+	FROM
+	  PersonalCTE
+	ORDER BY
+	  level, personal_id;
+
+SELECT * FROM v_personal_hierarchie_rec;
